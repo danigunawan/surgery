d848b9fef87e004c220a0001ef221920b9b6c6ba
Maksim Kryuchkov <mkryuchkov@devexperts.com>
2020-02-23 23:36:39 +0300

    added detect.py and readme

--- README.md
+++ README.md
@@ -1,1 +1,51 @@
-# surgery
+# Key Features Detection in Operating Room
+
+Operating Room i.e. OR is a room where a surgery takes place. 
+
+## Related work
+
+#### Surgeon face tracking
+
+Real-Time Visual Tracking of the Surgeon’s Face for Laparoscopic Surgery
+
+Irise, marker
+
+### 2018
+
+#### Iterative self-supervised Approach for Face Detection in the OR
+
+The idea is to fine-tune existing state of the art face detection models to the specific
+problem of face detection in OR.
+
+1. Generation of unlabeled dataset
+
+20k images generated from videos captured in the OR. Test videos and train videos were collected on different days to avoid overlapping.
+OpenPose model (a multi-person pose estimator) is used to get the approximate number of persons in each frame. Computations are rather efficient.
+Images are divided into categories with respect to the number of people detected. The images with the highest confidence (provided with OpenPose) are selected.
+
+
+2. Iterative refinement using a self-supervised approach
+
+
+
+
+
+## Used technologies
+
+### Face detection
+
+####  Haar cascade
+
+Introduced by Viola Jones. It is a classifier trained with given some input faces and non-faces. 
+Basic idea is to extract features from an image via special filters similar to CNN kernels.
+We traverse an image with a window of the fixed size and apply filters.
+
+
+### Face recognition
+
+The objective is to classify detected faces into doctor faces and patient faces. Doctor faces
+have some comprehensive features such as mask and gown while the patient faces do not.
+Тем не менее на пациенте может быть кислородная маска.
+
+To solve the problem I suggest using face embeddings + deep neural networks.
+That is how we are going to train our classifier i.e. recognition model. Siamese networks???
---
+++ detect.py
@@ -0,0 +1,178 @@
+import numpy as np
+import cv2
+from enum import Enum, auto
+from imutils.video import VideoStream
+
+import datetime
+
+from essh.essh_detector import ESSHDetector
+
+
+# detector = cv2.dnn.readNetFromCaffe(protoPath, modelPath)
+
+
+def init_default():
+    emModelPath = 'opencv-face-recognition/face_detection_model'
+    protoPath = emModelPath + '/deploy.prototxt'
+    # protoPath = './surgery/models/deploy.prototxt'
+    modelPath = emModelPath + '/res10_300x300_ssd_iter_140000.caffemodel'
+
+
+def init_faster_rcnn():
+    modelPath = './surgery/models/vgg16_faster_rcnn_iter_80000.caffemodel'
+    # protoPath = './surgery/models/deploy_frcnn.prototxt'
+    protoPath = './surgery/face-py-faster-rcnn/models/face/VGG16/faster_rcnn_end2end/solver.prototxt'
+
+
+def init_ssh():
+    modelPath = './surgery/SSH/data/SSH_models/SSH.caffemodel'
+    protoPath = './surgery/SSH/SSH/models/solver_ssh.prototxt'
+
+
+def detect_faces(video, output, w, h, detect_faces_on_img):
+    out = cv2.VideoWriter(output, cv2.VideoWriter_fourcc(*'MJPG'), 10, (w, h))
+
+    result_video = []
+    vs = cv2.VideoCapture(video)
+    i = 0
+
+    while True and i < 1000:
+        frame = vs.read()[1]
+
+        if frame is None:
+            break
+
+        if i % 3 == 0:
+            frame = detect_faces_on_img(frame)
+            result_video.append(frame)
+            out.write(frame)
+        i += 1
+
+    for frame in result_video:
+        cv2.imshow('surgery', frame)
+        key = cv2.waitKey(100)
+
+        if key == ord('q'):
+            break
+
+    out.release()
+    cv2.destroyAllWindows()
+
+
+class DSFDModel:
+    def __init__(self):
+        self.net = cv2.dnn.readNetFromTorch('FaceDetection-DSFD/weights/WIDERFace_DSFD_RES152.pth')
+
+
+    def forward(self, x):
+        return self.net(x)
+        
+
+
+class ESSHModel:
+    def __init__(self):
+        self.total_time = 0
+        self.scales = [1200, 1600]
+        self.t = 10
+        self.esshDetector = ESSHDetector('./essh/model/essh-r50', 0, ctx_id=0)
+
+    def detect_faces(self, video, output):
+        self.total_time = 0
+        detect_faces(video, output, 640, 360, self.detect_faces_on_img)
+        return self.total_time
+
+    def detect_faces_on_img(self, img):
+        im_shape = img.shape
+        target_size = self.scales[0]
+        max_size = self.scales[1]
+        im_size_min = np.min(im_shape[0:2])
+        im_size_max = np.max(im_shape[0:2])
+        if im_size_min > target_size or im_size_max > max_size:
+            im_scale = float(target_size) / float(im_size_min)
+            # prevent bigger axis from being more than max_size:
+            if np.round(im_scale * im_size_max) > max_size:
+                im_scale = float(max_size) / float(im_size_max)
+            img = cv2.resize(img, None, None, fx=im_scale, fy=im_scale)
+            print('resize to', img.shape)
+
+        # for i in range(t - 1):  # warmup
+        #     faces = esshDetector.detect(img)
+        timea = datetime.datetime.now()
+
+        faces = self.esshDetector.detect(img, threshold=0.5)
+        # print("DETECTED!!!!!!!")
+        bbox = np.round(faces[:, 0:5])
+        landmark = faces[:, 5:15].reshape(-1, 5, 2)
+
+        timeb = datetime.datetime.now()
+        diff = timeb - timea
+        diff = diff.total_seconds()
+        self.total_time += diff
+        # print('detection uses', diff.total_seconds(), 'seconds')
+        # print('find', faces.shape[0], 'faces')
+
+        for b in bbox:
+            cv2.rectangle(img, (int(b[0]), int(b[1])), (int(b[2]), int(b[3])), (0, 255, 0), 2)
+        for p in landmark:
+            for i in range(5):
+                cv2.circle(img, (p[i][0], p[i][1]), 1, (0, 0, 255), 2)
+        return img
+
+
+class DefaultModel:
+    def __init__(self):
+        emModelPath = 'opencv-face-recognition/face_detection_model'
+        protoPath = emModelPath + '/deploy.prototxt'
+        # protoPath = './surgery/models/deploy.prototxt'
+        modelPath = emModelPath + '/res10_300x300_ssd_iter_140000.caffemodel'
+        self.detector = cv2.dnn.readNetFromCaffe(protoPath, modelPath)
+        self.number = 4
+
+    def detect_faces(self, video, output):
+        detect_faces(video, output, 300, 300, self.detect_faces_on_img)
+        return None
+
+    def detect_faces_on_img(self, image):
+        image = cv2.resize(image, (300, 300))
+        (h, w) = image.shape[:2]
+        imageBlob = cv2.dnn.blobFromImage(image, 1.0, (w, h),
+                                          (104.0, 177.0, 123.0), swapRB=False, crop=False)
+
+        self.detector.setInput(imageBlob)
+        detections = self.detector.forward()
+
+        for detection in detections[0, 0, 0:self.number, 3:7]:
+            box = detection * np.array([w, h, w, h])
+            (startX, startY, endX, endY) = box.astype('int')
+
+            cv2.rectangle(image, (startX, startY), (endX, endY), (0, 255, 0), 2)
+        return image
+
+
+
+
+VIDEO_ROOT = 'videos/'
+IMAGE_ROOT = 'images/'
+
+
+def make_path(img_name):
+    return IMAGE_ROOT + img_name
+
+
+FIRST_VIDEO = 'videos/v1.mp4'
+BABY_VIDEO = 'videos/baby2.mp4'
+BABY_VIDEO_1 = 'videos/baby1.mp4'
+PATIENT_TRANSFER = 'videos/patient_transfer_cut.mp4'
+
+
+# defaultModel = DefaultModel()
+# defaultModel.detect_faces(BABY_VIDEO, 'baby_default.avi')
+
+# esshModel = ESSHModel()
+# total_time = esshModel.detect_faces(PATIENT_TRANSFER, 'patient_transfer_essh.avi')
+# print('total time: {} seconds'.format(total_time))
+
+dsfdModel = DSFDModel()
+result = dsfdModel.forward(cv2.imread(IMAGE_ROOT + 'lera.png'))
+
+print(result)
