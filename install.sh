9bbe0ba4d46809e8cb40dfd07bc05d36924af046
Maksim Kryuchkov <mkryuchkov@devexperts.com>
2020-02-24 12:48:11 +0300

    install.sh fixed

--- install.sh
+++ install.sh
@@ -21,6 +21,7 @@ if (ls model | grep R50); then
   echo "model already downloaded"
 else
   wget https://www.dropbox.com/s/53ftnlarhyrpkg2/retinaface-R50.zip
+  mkdir -p model
   mv retinaface-R50.zip model
   cd model
   unzip retinaface-R50.zip
