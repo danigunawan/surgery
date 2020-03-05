623818ecb0430ce1e758e29054bf963f1d223277
Maksim Kryuchkov <mkryuchkov@devexperts.com>
2020-03-05 14:00:34 +0300

    minor fixes

--- common.py
+++ common.py
@@ -14,7 +14,7 @@ def detect_faces(video, output, w, h, detect_faces_on_img):
             break
 
         if i % 3 == 0:
-            frame = detect_faces_on_img(frame)
+            frame, _ = detect_faces_on_img(frame)
             out.write(frame)
         i += 1
 
@@ -35,15 +35,15 @@ def detect_faces_with_trackers(video, output, w, h, detect_faces_on_img, tracker
 
         if i % 10 == 0:
             frame, boxes = detect_faces_on_img(frame)
-        #     for box in boxes:
-        #         trackers.add(cv2.TrackerKCF_create(), frame, box)
-        # else:
-        #     (success, boxes) = trackers.update(frame)
-        #
-        #     # loop over the bounding boxes and draw then on the frame
-        #     for box in boxes:
-        #         (x, y, w, h) = [int(v) for v in box]
-        #         cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
+            for box in boxes:
+                trackers.add(cv2.TrackerKCF_create(), frame, box)
+        else:
+            (success, boxes) = trackers.update(frame)
+
+            # loop over the bounding boxes and draw then on the frame
+            for box in boxes:
+                (x, y, w, h) = [int(v) for v in box]
+                cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
 
         out.write(frame)
         i += 1
